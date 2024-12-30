0e72ea66c48ab22ffcbeda0c8f2a62c5cecaf454
Alessio Sferro <sferro.alessio@gmail.com>
2024-12-30 07:41:27 +0100

    feat: add pre-commit prettier format

--- ui/src/context/App.context.tsx
+++ ui/src/context/App.context.tsx
@@ -1,40 +1,43 @@
-import {createContext, Dispatch, ReactNode, useReducer} from "react";
-
-interface AppContextModel {
-    state: AppState;
-    dispatch: Dispatch<AppState>
-}
+import { createContext, Dispatch, ReactNode, useReducer } from 'react';
 
 const initialState: AppState = {
-    animationStatus: "normal"
-}
+  animationStatus: 'normal',
+};
 
 const AppContext = createContext<AppContextModel>({
-    state: initialState,
-    dispatch: () => initialState
+  state: initialState,
+  dispatch: () => initialState,
 });
 
-interface AppState {
-    animationStatus: "normal" | "played";
+export const AppContextProvider = ({
+  children,
+  ...props
+}: AppContextProviderProps) => {
+  const [state, dispatch] = useReducer<AppReducer>(
+    (state, action) => ({ ...state, ...action }),
+    props
+  );
+
+  return (
+    <AppContext.Provider value={{ state, dispatch }}>
+      {children(state)}
+    </AppContext.Provider>
+  );
+};
+
+export default AppContext;
+
+interface AppContextModel {
+  state: AppState;
+  dispatch: Dispatch<AppState>;
+}
+
+export interface AppState {
+  animationStatus: 'normal' | 'played';
 }
 
 type AppReducer = (state: AppState, action: AppState) => AppState;
 
 interface AppContextProviderProps extends AppState {
-    children: (state: AppState) => ReactNode;
+  children: (state: AppState) => ReactNode;
 }
-
-export const AppContextProvider = ({children, ...props}: AppContextProviderProps) => {
-    const [state, dispatch] = useReducer<AppReducer>(
-        (state, action) => ({...state, ...action}),
-        props
-    );
-
-    return (
-        <AppContext.Provider value={{state, dispatch}}>
-            {children(state)}
-        </AppContext.Provider>
-    );
-}
-
-export default AppContext;
